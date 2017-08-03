ab32357db7e89acb6063ec271e63b5472792fc7e
oguayasa <oguayasa@gmail.com>
2017-08-03 14:28:54 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+
+setup(
+    name='tobii_pro_wrapper',
+    version='0.1',
+    author='Olivia Guayasamin',
+    author_email='oguayasa@gmail.com',
+    packages=['tobii_pro_wrapper'],
+    url='https://github.com/oguayasa/tobii_pro_wrapper',
+    license='LICENSE.txt',
+    description='Wrapper for the new Tobii Pro SDK',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'psychopy',
+        'datetime',
+        'random',
+        'collections',
+        'win32api',
+        'tobii_research',
+    ],
+)
